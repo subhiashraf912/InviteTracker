68fd1fca9c0309971cd5ab74a6df5df0cb6adb15
subhi.ashraf912 <subhi.ashraf912@gmail.com>
2022-05-24 22:15:19 +0300

    Edited Invite interface

--- dist/src/invite.d.ts
+++ dist/src/invite.d.ts
@@ -1,4 +1,3 @@
-import { InviteTargetType } from "discord-api-types";
 import { Client, Guild, GuildChannel, IntegrationApplication, InviteGuild, InviteStageInstance, PartialGroupDMChannel, User } from "discord.js";
 export default class invite {
     channel: GuildChannel | PartialGroupDMChannel;
@@ -17,10 +16,14 @@ export default class invite {
     presenceCount: number;
     stageInstance: InviteStageInstance | null;
     targetApplication: IntegrationApplication | null;
-    targetType: any;
+    targetType: InviteTargetType | null;
     targetUser: User | null;
     temporary: boolean | null;
     constructor(options: inviteOptions);
+}
+declare const enum InviteTargetType {
+    STREAM = 1,
+    EMBEDDED_APPLICATION = 2
 }
 interface inviteOptions {
     channel: GuildChannel | PartialGroupDMChannel;
